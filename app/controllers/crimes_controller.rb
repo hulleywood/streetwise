504121302d8a6570094b367ee19a60b3a0923d9b
James Hulley <james.hulley@gmail.com>
2014-06-03 18:33:12 -0700

    Added route for crimes near a midpoint server side   TODO: add ajax call after possible routes are generated

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -1,7 +1,9 @@
 class CrimesController < ApplicationController
   def near_crimes
-    # take midpoint and radius
-    # return crimes "in the box"
     @crimes = Crime.get_near_crimes(params[:midpoint], params[:radius])
+    @crimes.map! { |crime| { k: crime.y.to_i, A: crime.x.to_i } }
+
+    content_type :json
+    @crimes.to_json
   end
 end
