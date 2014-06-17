406031c5a95376831f835b8d42961877a7d64b02
James <james.hulley@gmail.com>
2014-06-17 11:18:21 -0700

    Map fits full screen

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
   searchController = new SearchController();
   $('form#directions').submit(searchController.initiateDirectionSearch.bind(searchController));
+  $(window).resize();
 });
+
+$(window).resize(function(){
+  var navHeight = $(".navbar").height()
+    + parseInt($(".navbar").css("margin-bottom"))
+    + parseInt($(".navbar").css("margin-top"))
+
+  var searchHeight = $("#directions").height()
+    + parseInt($("#directions").css("margin-bottom"))
+    + parseInt($("#directions").css("margin-top"))
+
+  var mapMargin = parseInt($('#map-canvas').css("margin-bottom"))
+  var windowHeight = $(window).height()
+  var height = windowHeight - navHeight - searchHeight
+    - mapMargin - 5
+
+  $('#map-canvas').height( height );
+})
