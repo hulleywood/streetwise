227a5afdf75ae62ae3120ee991574f64fd87bae3
James <james.hulley@gmail.com>
2014-06-18 10:01:52 -0700

    Removed local config for neo4j

--- config/initializers/neography.rb
+++ config/initializers/neography.rb
@@ -3,19 +3,3 @@ neo4j_url = ENV["GRAPHENEDB_URL"] || "http://localhost:7474" # default to local 
 uri = URI.parse(neo4j_url)
 
 require 'neography'
-
-Neography.configure do |config|
-  config.protocol       = "http://"
-  config.server         = "localhost"
-  config.port           = 7474
-  config.directory      = ""  # prefix this path with '/' 
-  config.cypher_path    = "/cypher"
-  config.gremlin_path   = "/ext/GremlinPlugin/graphdb/execute_script"
-  config.log_file       = "neography.log"
-  config.log_enabled    = false
-  config.max_threads    = 20
-  config.authentication = nil  # 'basic' or 'digest'
-  config.username       = nil
-  config.password       = nil
-  config.parser         = MultiJsonParser
-end
