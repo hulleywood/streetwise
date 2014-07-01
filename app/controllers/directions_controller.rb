9217b8649192f6f51df2d1c74f66505f69bf49a7
James <james.hulley@gmail.com>
2014-07-01 11:46:47 -0700

    Added status flags

--- app/controllers/directions_controller.rb
+++ app/controllers/directions_controller.rb
@@ -1,9 +1,15 @@
 class DirectionsController < ApplicationController
   def show
+    tstart = Time.now
+    puts "#{Time.now} Starting request on server..."
     @direction = Direction.new(params)
+    puts "#{Time.now} Direction instance created..."
     if valid_request
       response = @direction.gen_paths
       if response
+        tend = Time.now
+        puts "#{Time.now} Response object generated, sending..."
+        puts "Total time to complete request: #{tend - tstart}"
         render json: response
       else
         render json: "Something went wrong, please try again", status: 422
