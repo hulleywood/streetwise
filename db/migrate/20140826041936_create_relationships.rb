41a2294416cd5f9f60f7e74a4c732c88ed50514b
James <james.hulley@gmail.com>
2014-08-25 21:26:33 -0700

    Added rel migration

---
+++ db/migrate/20140826041936_create_relationships.rb
@@ -0,0 +1,15 @@
+class CreateRelationships < ActiveRecord::Migration
+  def change
+    create_table :relationships do |t|
+      t.belongs_to :node, as: :start_node
+      t.belongs_to :node, as: :end_node
+      t.decimal :crime_rating
+      t.decimal :distance
+      t.decimal :gradient
+      t.decimal :weight_safest_12
+      t.decimal :weight_safest_14
+      t.decimal :weight_safest_18
+      t.decimal :weight_shortest
+    end
+  end
+end
